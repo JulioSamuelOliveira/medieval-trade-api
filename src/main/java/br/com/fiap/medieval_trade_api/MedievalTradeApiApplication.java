1f2c65781c23cf92432c21130a9c3dd3b1267319
JulioSamuelOliveira <juliosamuoliveira@gmail.com>
2025-04-15 16:12:56 -0300

    inicial commit

---
+++ src/main/java/br/com/fiap/medieval_trade_api/MedievalTradeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.medieval_trade_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedievalTradeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedievalTradeApiApplication.class, args);
+	}
+
+}
